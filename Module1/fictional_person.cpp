c9065199830a135f0b9889d81ecfb78ad07cc3bd
Brian Salchert <brisalchert@gmail.com>
2024-06-12 12:21:32 -0400

    Created fictional_person.cpp

---
+++ Module1/fictional_person.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string firstName = "John",
+           lastName = "Smith",
+           address = "901 Main Street",
+           city = "Boston";
+    int zipCode = 02112;
+
+    // Print personal information to the console
+    cout << "PERSONAL INFORMATION" << endl;
+    cout << "--------------------" << endl;
+    cout << "First Name: " << firstName << endl;
+    cout << "Last Name: " << lastName << endl;
+    cout << "Address: " << address << endl;
+    cout << "City: " << city << endl;
+    cout << "Zip Code: " << zipCode << endl;
+
+    return 0;
+}
