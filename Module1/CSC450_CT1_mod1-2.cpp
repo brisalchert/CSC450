80a4048537d245744cf6e55c486067c4cb0fd433
Brian Salchert <brisalchert@gmail.com>
2024-06-12 12:41:09 -0400

    Corrected cout errors

--- Module1/CSC450_CT1_mod1-2.cpp
+++ Module1/CSC450_CT1_mod1-2.cpp
@@ -1,27 +1,23 @@
-
-
 #include<iostream>
 #include<conio.h>
 
-Standard namespace declaration
+// Standard namespace declaration
 using namespace std;
 
-Main Function
+// Main Function
 int main()
 {
-
      double myMoney = 1000.50;  //this should be printed out
 
+     // Standard Ouput Statement
+     cout << "Please be sure to correct all syntax errors in this program" << endl;
 
-     Standard Ouput Statement
-     cout"Please be sure to correct all syntax errors in this program"<<endl;
+     cout << "I have corrected all errors for this program." << endl;
 
-     cout<<"I have corrected all errors for this program. <<endl;
-
-     cout<<" The total amount of money available is = "<<   <<endl;
+     cout << "The total amount of money available is = " << myMoney << endl;
 
      // Wait For Output Screen
 
-     Main Function return Statement
+     // Main Function return Statement
      return 0;
 }
