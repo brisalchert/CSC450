0007950b1f92aeab354d20bcc2efed18c0b5a0c0
Brian Salchert <brisalchert@gmail.com>
2024-06-20 23:33:17 -0400

    Added a loop to concatenate three times

--- Module2/string_concatenation.cpp
+++ Module2/string_concatenation.cpp
@@ -3,21 +3,30 @@
 using namespace std;
 
 int main() {
-    // Use strings instead of character arrays to avoid buffer overflow
-    string firstString;
-    string secondString;
+    // Run for three sets of user input
+    for (int i = 0; i < 3; i++) {
+        // Identify the input set number
+        cout << "Concatenation Number " << i + 1 << ":" << endl;
 
-    // Get input from the user
-    cout << "Enter the first string: " << endl;
-    cin >> firstString;
-    cout << "Enter the second string: " << endl;
-    cin >> secondString;
+        // Use strings instead of character arrays to avoid buffer overflow
+        string firstString;
+        string secondString;
 
-    // Concatenate the strings by appending secondString to firstString
-    firstString.append(secondString);
+        // Get input from the user
+        cout << "Enter the first string: " << endl;
+        cin >> firstString;
+        cout << "Enter the second string: " << endl;
+        cin >> secondString;
 
-    // Print the results to the user
-    cout << "Concatenated string: \n" + firstString << endl;
+        // Concatenate the strings by appending secondString to firstString
+        firstString.append(secondString);
+
+        // Print the results to the user
+        cout << "Concatenated string: \n" + firstString << endl;
+
+        // Print a newline to separate sets of input
+        cout << endl;
+    }
 
     return 0;
 }
