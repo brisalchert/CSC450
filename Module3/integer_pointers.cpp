2efcc005184051f228e6ef52cca15e5eb2ccc907
Brian Salchert <brisalchert@gmail.com>
2024-06-24 12:06:17 -0400

    Added check for successful dynamic memory allocation

--- Module3/integer_pointers.cpp
+++ Module3/integer_pointers.cpp
@@ -9,38 +9,50 @@ int main() {
     int *pointer3;
 
     // Initialize pointers to dynamic memory
-    pointer1 = new int;
-    pointer2 = new int;
-    pointer3 = new int;
+    // Use (nothrow) to prevent exception, then check for nullptr
+    pointer1 = new (nothrow) int;
+    pointer2 = new (nothrow) int;
+    pointer3 = new (nothrow) int;
 
-    // Ask for user input, storing in dynamic memory
-    cout << "Enter the first integer: " << endl;
-    cin >> *pointer1;
+    // If dynamic memory allocation fails, terminate the program
+    if (pointer1 == nullptr || pointer2 == nullptr || pointer3 == nullptr) {
+        cout << "Dynamic memory allocation unsuccessful. Terminating program." << endl;
 
-    cout << "Enter the second integer: " << endl;
-    cin >> *pointer2;
+        // Ensure dynamic memory is deallocated
+        delete pointer1;
+        delete pointer2;
+        delete pointer3;
+    }
+    else {
+        // Ask for user input, storing in dynamic memory
+        cout << "Enter the first integer: " << endl;
+        cin >> *pointer1;
 
-    cout << "Enter the third integer: " << endl;
-    cin >> *pointer3;
+        cout << "Enter the second integer: " << endl;
+        cin >> *pointer2;
 
-    cout << endl;
+        cout << "Enter the third integer: " << endl;
+        cin >> *pointer3;
 
-    // Display contents of the variables using dereference operator
-    cout << "Contents of first variable: " << *pointer1 << endl;
-    cout << "Contents of second variable: " << *pointer2 << endl;
-    cout << "Contents of third variable: " << *pointer3 << endl;
+        cout << endl;
 
-    cout << endl;
+        // Display contents of the variables using dereference operator
+        cout << "Contents of first variable: " << *pointer1 << endl;
+        cout << "Contents of second variable: " << *pointer2 << endl;
+        cout << "Contents of third variable: " << *pointer3 << endl;
 
-    // Display contents of the pointers using pointer names
-    cout << "Contents of first pointer: " << pointer1 << endl;
-    cout << "Contents of second pointer: " << pointer2 << endl;
-    cout << "Contents of third pointer: " << pointer3 << endl;
+        cout << endl;
 
-    // Deallocate dynamic memory
-    delete pointer1;
-    delete pointer2;
-    delete pointer3;
+        // Display contents of the pointers using pointer names
+        cout << "Contents of first pointer: " << pointer1 << endl;
+        cout << "Contents of second pointer: " << pointer2 << endl;
+        cout << "Contents of third pointer: " << pointer3 << endl;
+
+        // Deallocate dynamic memory
+        delete pointer1;
+        delete pointer2;
+        delete pointer3;
+    }
 
     return 0;
 }
