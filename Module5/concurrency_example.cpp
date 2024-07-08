3fae71adf7930d819287a5801e1b14620b95b055
Brian Salchert <brisalchert@gmail.com>
2024-07-08 11:54:30 -0400

    Created concurrency_example.cpp

---
+++ Module5/concurrency_example.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+
+using namespace std;
+
+// Declare a mutex to use in lock_guard
+mutex incrementMutex;
+
+// Initialize an integer to increment
+int value = 0;
+
+void incrementHundredThousand() {
+    // lock_guard<mutex> lockGuard(incrementMutex);
+
+    // Increment the value one hundred thousand times
+    for (int i = 0; i < 100000; i++) {
+        value++;
+    }
+}
+
+void decrementFiftyThousand() {
+    // lock_guard<mutex> lockGuard(incrementMutex);
+
+    // Decrement the value fifty thousand times
+    for (int i = 0; i < 50000; i++) {
+        value--;
+    }
+}
+
+int main() {
+    // Initialize several threads for executing the function
+    thread thread1(incrementHundredThousand);
+    thread thread2(decrementFiftyThousand);
+    thread thread3(incrementHundredThousand);
+
+    thread1.join();
+    thread2.join();
+    thread3.join();
+
+    // Print the resulting value
+    cout << "Resulting value: " << value << endl;
+
+    return 0;
+}
