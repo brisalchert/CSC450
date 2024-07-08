4307d1cff7947d15ebcb465ebaa0ab30c0211887
Brian Salchert <brisalchert@gmail.com>
2024-07-08 14:23:35 -0400

    Added method for reversing text file

--- Module5/file_access.cpp
+++ Module5/file_access.cpp
@@ -1,23 +1,46 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
+void createReverseFile(string filename) {
+    ifstream readFile(filename);
+    ofstream writeFile("CSC450-mod5-reverse.txt");
+
+    if (readFile.is_open() && writeFile.is_open()) {
+        istreambuf_iterator<char> input_start(readFile);
+        istreambuf_iterator<char> input_end;
+        ostreambuf_iterator<char> output_start(writeFile);
+        vector<char> input_data(input_start, input_end);
+
+        reverse_copy(input_data.begin(), input_data.end(), output_start);
+    }
+    else {
+        cout << "Unable to open read and/or write files";
+    }
+}
+
 int main() {
     string userInput;
+    string filename = "CSC450_CT5_mod5.txt";
     fstream file;
 
     // Obtain input from user
     cout << "Enter a string: ";
     getline(cin, userInput);
 
-    // Append the user input to the end of CSC450_CT5_mod5.txt
-    file.open("CSC450_CT5_mod5.txt", ios::app);
+    // Open the existing text file
+    file.open(filename, ios::app);
     if (file.is_open()) {
-        string line;
+        // Append the user input to the end of CSC450_CT5_mod5.txt
         file << userInput;
         file.close();
+
+        // Reverse the file contents and store in CSC450-mod5-reverse.txt
+        createReverseFile(filename);
     }
     else {
         cout << "Unable to open file";
