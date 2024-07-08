d89d4ec542b623caf600c0d52da06025d9d47f05
Brian Salchert <brisalchert@gmail.com>
2024-07-08 13:53:26 -0400

    Appended user input to the end of the file

--- Module5/file_access.cpp
+++ Module5/file_access.cpp
@@ -6,10 +6,22 @@ using namespace std;
 
 int main() {
     string userInput;
+    fstream file;
 
     // Obtain input from user
     cout << "Enter a string: ";
     getline(cin, userInput);
+
+    // Append the user input to the end of CSC450_CT5_mod5.txt
+    file.open("CSC450_CT5_mod5.txt", ios::app);
+    if (file.is_open()) {
+        string line;
+        file << userInput;
+        file.close();
+    }
+    else {
+        cout << "Unable to open file";
+    }
 
     return 0;
 }
