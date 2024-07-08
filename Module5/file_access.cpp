e9fcdc4b32cdb696a9a9827e4221179232b14bc3
Brian Salchert <brisalchert@gmail.com>
2024-07-08 12:41:57 -0400

    Created file_access.cpp with input prompt

---
+++ Module5/file_access.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string userInput;
+
+    // Obtain input from user
+    cout << "Enter a string: ";
+    getline(cin, userInput);
+
+    return 0;
+}
