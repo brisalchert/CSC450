47754291cd16f2d498b6cd2009625ea84a2c069e
Brian Salchert <brisalchert@gmail.com>
2024-07-22 13:11:01 -0400

    Created PaintColor.java

---
+++ Module7/PaintColor.java
@@ -0,0 +1,30 @@
+package Module7;
+
+public class PaintColor {
+    private String color;
+
+    // Constructor initializes color to "red"
+    public PaintColor() {
+        this.color = "red";
+    }
+
+    // Accessor for color
+    public String getColor() {
+        return color;
+    }
+
+    // Method to set color to "green"
+    public void setColorGreen() {
+        this.color = "green";
+    }
+
+    // Method to set color to "blue"
+    public void setColorBlue() {
+        this.color = "blue";
+    }
+
+    // Method to set color to "red"
+    public void setColorRed() {
+        this.color = "red";
+    }
+}
