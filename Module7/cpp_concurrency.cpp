a83152331b98fb5fee6bc2300ac21b72dfee1346
Brian Salchert <brisalchert@gmail.com>
2024-07-24 14:42:11 -0400

    Added condition variable for proper order of execution

--- Module7/cpp_concurrency.cpp
+++ Module7/cpp_concurrency.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 #include <thread>
 #include <mutex>
+#include <condition_variable>
 
 using namespace std;
 
-// Declare a mutex to use in lock_guard
+// Declare a mutex to use in lock_guard and unique_lock
 mutex countMutex;
+
+// Initialize a condition variable and predicate
+condition_variable cond;
+bool threadOneDone = false;
 
 // Initialize the shared counter to 0
 int counter = 0;
@@ -27,12 +32,21 @@ void countUp() {
     }
 
     cout << endl;
+
+    // Notify the other thread to "wake up"
+    threadOneDone = true;
+    cond.notify_one();
 }
 
 // Method for counting down to 0
 void countDown() {
-    // Set a lock using the mutex, which will automatically unlock at the end of the method
-    lock_guard<mutex> lockguard(countMutex);
+    // Set a unique lock using the mutex, which will automatically unlock at the end of the method
+    unique_lock<mutex> uniqueLock(countMutex);
+
+    // Wait on the condition variable for other thread to finish
+    while (!threadOneDone) {
+        cond.wait(uniqueLock);
+    }
 
     cout << "Counting down to 0..." << endl;
 
